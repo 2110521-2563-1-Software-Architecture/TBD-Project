3c922aff7aeaf0cdcb92baa8274e1d3f1871db86
anawat <alonypau@gmail.com>
2020-09-27 15:13:41 +0700

    add path
    /login and /register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,25 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import Login from './Components/Login'
+import Register from './Components/Register'
 import axios from 'axios'
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path={'/login'}>
+            <Login/>
+          </Route>
+
+          <Route path={'/register'}>
+            <Register/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
