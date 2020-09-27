d8e1b17bdd3e27f5cbfc58407484bfb551a9035d
anawat <alonypau@gmail.com>
2020-09-27 22:01:41 +0700

    change name Register.js to Signup.js
    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import Login from './Components/Login'
-import Register from './Components/Register'
+import Signup from './Components/Signup'
 import axios from 'axios'
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 import "antd/dist/antd.css";
@@ -16,8 +16,8 @@ function App() {
             <Login/>
           </Route>
 
-          <Route path={'/register'}>
-            <Register/>
+          <Route path={'/signup'}>
+            <Signup/>
           </Route>
         </Switch>
       </div>
