a75ff283da1edf30d3277ba279892600527d6600
Keerati Chuatanapinyo <keeratipond1203@gmail.com>
2020-10-20 19:53:40 +0700

    555

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,19 +1,67 @@
 import React from 'react'
-import {Row, Col} from 'antd'
+import {Row, Col, Table, Tag, Space, Alert } from 'antd'
 
 function Home() {
     return (
         <div>
             <Row >
-                <Col style={Style} span={6}>col-6</Col>
-                <Col style={Style} span={12}>col-12</Col>
-                <Col style={Style} span={6}>col-6</Col>
+                <Col style={Style} span={6}>
+                    <div className="picture&name">
+                        My Name is Thanapun(กาก)
+                    </div>
+                    <div className="FriendList" style={FriendList}>
+                        <Table columns={columns} dataSource={data} />
+                    </div>
+                </Col>
+                <Col style={Style} span={12}>
+                    ไว้ใส่ contents
+                </Col>
+                <Col style={Style} span={6}>
+                    <div className="All User" style={AllUser}>
+                        <Table columns={column} dataSource={dat} />
+                    </div>
+                </Col>
             </Row>
         </div>
     )
 }
-
+/////////////Friend List///////////////////////
+const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    }
+    
+]
+const data = [
+    {
+      name: "fffm  kg"
+    }
+]
+//////////////All User////////////////////////
+const column = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    }
+    
+]
+const dat = [
+    {
+      name: "fffm  kg"
+    }
+]
+////////////////////////////////////////////////
 const Style = {
     border: "solid black"
+}
+const FriendList = {
+    width: "ๅจ0%",
+    margin: 'auto'
+}
+const AllUser = {
+    
 }
 export default Home
