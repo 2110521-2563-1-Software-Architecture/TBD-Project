6f05d453e5e97ecc38a9605bec4c1b848cc4dd52
Keerati Chuatanapinyo <keeratipond1203@gmail.com>
2020-10-22 20:31:25 +0700

    TT

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,69 +1,89 @@
-import React from 'react'
-import {Row, Col, Table, Tag, Space, Alert } from 'antd'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {Row, Col} from 'antd'
 import Default from '../picture/default.png'
 
+const FriendList = props => (
+    <tr>
+        <td>{props.list}</td>
+    </tr>
+)
+const AllUser = props => (
+    <tr>
+        <td>{props.list}</td>
+    </tr>
+)
+
 function Home() {
+    const [friendsList, setFriendsList] = useState([]);
+    const [allUser, setAllUser] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000')
+            .then(response => {
+                setFriendsList(response.data.friend)
+                setAllUser(response.data.user)
+            })
+            .catch(function (error){
+                console.log(error);
+            })
+    }, [])
+
+    const Friend = () => {
+        return friendsList.map(function(currentlist, i){
+            return <FriendList list={currentlist} key={i} />;
+        })
+    }
+    const User = () => {
+        return allUser.map(function(currentlist, i){
+            return <AllUser list={currentlist} key={i} />;
+        })
+    }
     return (
         <div>
             <Row >
                 <Col style={Style} span={6}>
                     <div className="picture_name" style={{width: "90%", margin: "auto"}}>
                         <img src={Default} style={{width:"100%"}}/>
-                        My Name is Thanapun(กาก)
+                        My Name is Thanapun(ไม่กากและโหดมาก) <br/>
+                        เหลือตรงดึงรูปภาพจาก backend นะ(ตรงนี้) ทำไม่เป็น TT <br/>
+                        แล้วก็ฝากตรวจที่ทำไปด้วยนะ ไม่ค่อยเข้าใจเลย TT
                     </div>
-                    <div className="FriendList" style={FriendList}>
-                        <Table columns={columns} dataSource={data} />
+                    <div className="FriendList" style={{margin:"auto",width:"90%"}}>
+                        <table style={{width:"100%"}}>
+                            <tr>
+                                <th>Friend List</th>
+                            </tr>
+                            <tr>
+                                {Friend}
+                            </tr>
+                        </table>
+                        
                     </div>
                 </Col>
                 <Col style={Style} span={12}>
                     ไว้ใส่ contents
                 </Col>
                 <Col style={Style} span={6}>
-                    <div className="All User" style={AllUser}>
-                        <Table columns={column} dataSource={dat} />
+                    <div className="All User" style={{margin:"auto",width:"90%"}}>
+                        <table style={{width:"100%"}}>
+                            <tr>
+                                <th>All User</th>
+                            </tr>
+                            <tr>
+                                {User}
+                            </tr>
+                        </table>
                     </div>
                 </Col>
             </Row>
         </div>
     )
 }
-/////////////Friend List///////////////////////
-const columns = [
-    {
-      title: 'FriendList',
-      dataIndex: 'FriendList',
-      key: 'FriendList',
-    }
-    
-]
-const data = [
-    {
-      FriendList: "fffm  kg"
-    }
-]
-//////////////All User////////////////////////
-const column = [
-    {
-      title: 'AllUser',
-      dataIndex: 'AllUser',
-      key: 'AllUser',
-    }
-    
-]
-const dat = [
-    {
-      AllUser: "fffm  kg"
-    }
-]
+
 ////////////////////////////////////////////////
 const Style = {
     border: "solid black"
 }
-const FriendList = {
-    width: "ๅจ0%",
-    margin: 'auto'
-}
-const AllUser = {
 
-}
 export default Home
