edd8162164a53507947209c3093e3d7bfe1c1c42
Setthanan <42529434+Setthanan@users.noreply.github.com>
2020-11-15 23:03:44 +0700

    define rate

--- backend/app/models/news_feed.py
+++ backend/app/models/news_feed.py
@@ -158,8 +158,8 @@ class NewsFeed(BaseModel):
 #       score = 2
 #   if action == "love":
 #       score = 3
-#   u = max(old_score,rate * t)
-#   v = min(old_score,rate * t)
+#   u = max(old_score, t)
+#   v = min(old_score, t)
 #   return (u + math.log(math.exp(v-u))) * score
 
 #def affinity(self):
